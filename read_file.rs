6225c8a444a5d421f63ac7c111e52a16031590e9
Christopher Reid <chronic88@gmail.com>
2014-12-06 23:04:46 -0800

    serves multiple HTML pages, gives 404 if page doesn't exist

---
+++ read_file.rs
@@ -0,0 +1,27 @@
+use std::os;
+use std::io::BufferedReader;
+use std::io::File;
+
+fn main() {
+    for target in os::args().iter() {
+        scan_file(target);
+    }
+}
+
+fn scan_file(path_str: &String) {
+  let path = Path::new(path_str.as_bytes());
+  let file = File::open(&path);
+  let mut reader = BufferedReader::new(file);
+
+  for line in reader.lines() {
+      match line {
+          Ok(s) => {
+              match s.as_slice() {
+                  "```\n" => print!("<code>\n"),
+                  _       => print!("{}", s.as_slice()),
+              }
+          }
+          Err(_) => return,
+      }
+  }
+}
--- server.rs
+++ server.rs
@@ -5,37 +5,14 @@ use std::io::BufferedReader;
 use std::io::File;
 
 
-use std::str;
-
-//fn main() {
-//    for target in os::args().iter() {
-//        scan_file(target);
-//    }
-//}
-
-/*fn scan_file(path_str: &String) {
-    let path = Path::new(path_str.as_bytes());
-    let file = File::open(&path);
-    let mut reader = BufferedReader::new(file);
-
-    for line in reader.lines() {
-        match line {
-            Ok(s) => {
-                match s.as_slice() {
-                    "```\n" => print!("<code>\n"),
-                    _       => print!("{}", s.as_slice()),
-                }
-            }
-            Err(_) => return,
-        }
-    }
-}*/
-
 fn get_file_string(path_str: &String) -> String{
     let path = Path::new(path_str.as_bytes());
     let file = File::open(&path);
     let mut reader = BufferedReader::new(file);
-    reader.read_to_string().unwrap()
+    match reader.read_to_string() {
+        Ok(x) => x,
+        Err(_) => "err".to_string(),
+    }
 }
 
 // why doesn't this work?
@@ -55,18 +32,16 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8001");
     let mut acceptor = listener.listen();
 
-    let ref file_to_host = os::args()[1];
-    let html = get_file_string(file_to_host);
+ //   let ref file_to_host = os::args()[1];
+ //   let html = get_file_string(file_to_host);
 
-    fn handle_client(mut stream: TcpStream, to_host: String) {
+    fn handle_client(mut stream: TcpStream) {
  
         let header: String = "HTTP/1.1 200 OK\nContent-Type: text/html\n\r\n".to_string();
         let mut stream_reader = BufferedReader::new(stream.clone());
       
-//        get unpack_stream_buffer working
-//        println!("{}", unpack_stream_buffer(&stream_reader));
+        // read until first space of request to determine HTTP method
 
-        // read until first space to determine HTTP method
         let method = match stream_reader.read_until(32) {
             Ok(buffer) => match String::from_utf8(buffer) {
                 Ok(string) => string,
@@ -75,9 +50,8 @@ fn main() {
             Err(err) => err.to_string(),
         };
 
-        println!("method: {}", method);
+        // read until second space of request to get URI
 
-        // read until second space to get URI
         let request_uri = match stream_reader.read_until(32) {
             Ok(buffer) => match String::from_utf8(buffer) {
                 Ok(string) => string,
@@ -88,23 +62,26 @@ fn main() {
        
         println!("request uri: {}", request_uri);
         
+        // Redirect root to a.html, otherwise parse file name
+
         let get_file = match request_uri.as_slice() {
             "/ " => "a.html".to_string(),
-            _ => request_uri.as_slice().slice(1,request_uri.as_slice().len()).to_string(),
+            _ => request_uri.as_slice().slice(1,request_uri.as_slice().len()-1).to_string(),
         };
 
-        println!("{}", get_file);
+        // If file doesn't exist 404, otherwise serve file
 
-        stream.write_str((header + to_host).as_slice());
-        // drop(stream);
+        match get_file_string(&get_file).as_slice() {
+            "err" => stream.write_str((header + "404".to_string()).as_slice()).unwrap(),
+            _ => stream.write_str((header + get_file_string(&get_file)).as_slice()).unwrap(),
+        }
     }
 
     for stream in acceptor.incoming() {
-        let html = html.clone();
         match stream {
             Err(e) => { println!("{}", e) }
             Ok(stream) => spawn(proc() {
-                handle_client(stream,html)
+                handle_client(stream)
             })
         }
     }
