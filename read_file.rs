64f1fef4a0acce90f9ac61c80851bd112eb21cca
Christopher Reid <chronic88@gmail.com>
2014-12-07 00:47:53 -0800

    experiment with BufferedReader to read image

--- read_file.rs
+++ read_file.rs
@@ -12,16 +12,19 @@ fn scan_file(path_str: &String) {
   let path = Path::new(path_str.as_bytes());
   let file = File::open(&path);
   let mut reader = BufferedReader::new(file);
+    
+  println!("{}",reader.read_to_end());
 
-  for line in reader.lines() {
-      match line {
-          Ok(s) => {
-              match s.as_slice() {
-                  "```\n" => print!("<code>\n"),
-                  _       => print!("{}", s.as_slice()),
-              }
-          }
-          Err(_) => return,
-      }
-  }
+  
+//  for line in reader.lines() {
+//      match line {
+//          Ok(s) => {
+//              match s.as_slice() {
+//                  "```\n" => print!("<code>\n"),
+//                  _       => print!("{}", s.as_slice()),
+//              }
+//          }
+//          Err(_) => return,
+//      }
+//  }
 }
