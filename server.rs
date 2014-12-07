1288d8ebf9fcd8980edfbc552e9f0f2936789b82
Christopher Reid <chronic88@gmail.com>
2014-12-06 22:36:24 -0800

    successfully parsing requests

--- server.rs
+++ server.rs
@@ -38,6 +38,17 @@ fn get_file_string(path_str: &String) -> String{
     reader.read_to_string().unwrap()
 }
 
+// why doesn't this work?
+
+//fn unpack_stream_buffer<T>(stream_reader: &BufferedReader<T>) -> String {
+//    match stream_reader.read_until(32) {
+//         Ok(buffer) => match String::from_utf8(buffer) {
+//             Ok(string) => string,
+//             Err(err) => err.to_string(),
+//         },
+//         Err(err) => err.to_string(),
+//     }
+//}
 
 fn main() {
 
@@ -48,17 +59,42 @@ fn main() {
     let html = get_file_string(file_to_host);
 
     fn handle_client(mut stream: TcpStream, to_host: String) {
+ 
         let header: String = "HTTP/1.1 200 OK\nContent-Type: text/html\n\r\n".to_string();
-     //   match stream.read_to_string() {
-     //       Ok(x) => println!("{}", x),
-     //       Err(err) => println!("FAILURE"),
-     //   }
-
         let mut stream_reader = BufferedReader::new(stream.clone());
-        println!("{}", stream_reader.read_until(32));
-        println!("{}", stream_reader.read_until(46));
- //       println!("{}", String::from_utf8(stream.read_exact(12).unwrap()));
+      
+//        get unpack_stream_buffer working
+//        println!("{}", unpack_stream_buffer(&stream_reader));
+
+        // read until first space to determine HTTP method
+        let method = match stream_reader.read_until(32) {
+            Ok(buffer) => match String::from_utf8(buffer) {
+                Ok(string) => string,
+                Err(err) => err.to_string(),
+            },
+            Err(err) => err.to_string(),
+        };
+
+        println!("method: {}", method);
+
+        // read until second space to get URI
+        let request_uri = match stream_reader.read_until(32) {
+            Ok(buffer) => match String::from_utf8(buffer) {
+                Ok(string) => string,
+                Err(err) => err.to_string(),
+            },
+            Err(err) => err.to_string(),
+        };
+       
+        println!("request uri: {}", request_uri);
         
+        let get_file = match request_uri.as_slice() {
+            "/ " => "a.html".to_string(),
+            _ => request_uri.as_slice().slice(1,request_uri.as_slice().len()).to_string(),
+        };
+
+        println!("{}", get_file);
+
         stream.write_str((header + to_host).as_slice());
         // drop(stream);
     }
